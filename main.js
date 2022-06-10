289f85d6fab4fc1c2f16ec366034dbbab7f2d266
dmars <dmars8047@live.com>
2022-06-09 22:54:39 -0400

    Console styling

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function createMainWindow() {
   mainWindow.loadFile('main.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
